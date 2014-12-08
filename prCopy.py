53c2cfd7699c29d7523d6b549d41415c07e9ba05
Edward Guevara <edarague@gmail.com>
2014-12-08 16:37:54 -0500

    cdo_main and stats_temp updated

--- prCopy.py
+++ prCopy.py
@@ -5,12 +5,12 @@ from sys import argv
 if len(argv) != 2:
     print "Usage: python prCopy.py MODEL"
     exit(1)
-MODEL = int(argv[1])
+MODEL = str(argv[1])
 
 for year in range(2006, 2100):
     fs = '/box0_p2/BCSD/%s/rcp85/day/r1i1p1/pr/pr_day_%s_rcp85_r1i1p1_%s.nc' % (MODEL, MODEL, year)
     ft = '/mnt/BCSD/%s/junk/prmm_day_%sMODEL_rcp45_r1i1p1_%s.nc' % (MODEL, MODEL, year)
-    if not path.exists(fs):
+    if not path.exists(ft):
         print '... copying %s' % ft
         system('cp %s %s' % (fs, ft))
     else:
