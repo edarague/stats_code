65e42c4b3db1a57ff5838cd2165efbe14ae5ba48
edarague <edarague@gmail.com>
2014-12-09 15:09:11 -0500

    Update prCopy.py

--- prCopy.py
+++ prCopy.py
@@ -9,8 +9,9 @@ MODEL = str(argv[1])
 
 for year in range(2006, 2100):
     fs = '/box0_p2/BCSD/%s/rcp85/day/r1i1p1/pr/pr_day_%s_rcp85_r1i1p1_%s.nc' % (MODEL, MODEL, year)
-    ft = '/mnt/BCSD/%s/junk/prmm_day_%s_rcp45_r1i1p1_%s.nc' % (MODEL, MODEL, year)
-    if not path.exists(ft):
+    fc = '/mnt/BCSD/%s/junk/prmm_day_%s_rcp45_r1i1p1_%s.nc' % (MODEL, MODEL, year)
+    ft = '/mnt/BCSD/%s/rcp85/day/r1i1p1/pr/pr_day_%s_rcp85_r1i1p1_%s.nc' % (MODEL, MODEL, year)
+    if not path.exists(fc):
         print '... copying %s' % ft
         system('cp %s %s' % (fs, ft))
     else:
