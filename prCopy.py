f7db5799e3f8dbddf0e44c1b7f0d7b87c95a1ae1
edarague <edarague@gmail.com>
2014-12-09 14:59:41 -0500

    Update prCopy.py

--- prCopy.py
+++ prCopy.py
@@ -2,14 +2,16 @@ __author__ = 'Edward Guevara'
 from os import system, path
 from sys import argv
 
-if len(argv) != 2:
-    print "Usage: python prCopy.py MODEL"
+if len(argv) != 4:
+    print "Usage: python prCopy.py iniYear finYear MODEL"
     exit(1)
-MODEL = str(argv[1])
+    iniYear = int(argv[1])
+    finYear = int(argv[2])
+    MODEL = str(argv[3])
 
-for year in range(2006, 2100):
+for year in range(iniYear, finYear + 1):
     fs = '/box0_p2/BCSD/%s/rcp85/day/r1i1p1/pr/pr_day_%s_rcp85_r1i1p1_%s.nc' % (MODEL, MODEL, year)
-    ft = '/mnt/BCSD/%s/junk/prmm_day_%sMODEL_rcp45_r1i1p1_%s.nc' % (MODEL, MODEL, year)
+    ft = '/mnt/BCSD/%s/junk/prmm_day_%s_rcp45_r1i1p1_%s.nc' % (MODEL, MODEL, year)
     if not path.exists(ft):
         print '... copying %s' % ft
         system('cp %s %s' % (fs, ft))
