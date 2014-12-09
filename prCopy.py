474592cda623559a38aecd19fa3bd55d86c8c610
edarague <edarague@gmail.com>
2014-12-09 15:20:38 -0500

    Update prCopy.py

--- prCopy.py
+++ prCopy.py
@@ -2,17 +2,18 @@ __author__ = 'Edward Guevara'
 from os import system, path
 from sys import argv
 
-if len(argv) != 2:
-    print "Usage: python prCopy.py MODEL"
+if len(argv) != 3:
+    print "Usage: python prCopy.py MODEL Scenario"
     exit(1)
 MODEL = str(argv[1])
+Scen = str(argv[2])
 
 for year in range(2006, 2100):
-    fs = '/box0_p2/BCSD/%s/rcp85/day/r1i1p1/pr/pr_day_%s_rcp85_r1i1p1_%s.nc' % (MODEL, MODEL, year)
+    fs = '/box0_p2/BCSD/%s/%s/day/r1i1p1/pr/pr_day_%s_%s_r1i1p1_%s.nc' % (MODEL, Scen, MODEL, Scen, year)
     print fs
-    fc = '/mnt/BCSD/%s/junk/prmm_day_%s_rcp45_r1i1p1_%s.nc' % (MODEL, MODEL, year)
+    fc = '/mnt/BCSD/%s/junk/prmm_day_%s_%s_r1i1p1_%s.nc' % (MODEL, MODEL, Scen, year)
     print fc
-    ft = '/mnt/BCSD/%s/rcp85/day/r1i1p1/pr/pr_day_%s_rcp85_r1i1p1_%s.nc' % (MODEL, MODEL, year)
+    ft = '/mnt/BCSD/%s/%s/day/r1i1p1/pr/pr_day_%s_%s_r1i1p1_%s.nc' % (MODEL, Scen, MODEL, Scen, year)
     print ft
     if not path.exists(fc):
         print '... copying %s' % ft
