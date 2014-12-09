086e3439e8ed22689efd4e2cc8581dd7c85fa110
edarague <edarague@gmail.com>
2014-12-09 16:03:59 -0500

    Update dummyFile.py

--- dummyFile.py
+++ dummyFile.py
@@ -2,15 +2,17 @@ __author__ = 'Edward Guevara'
 from os import system, path
 from sys import argv
 
-if len(argv) != 4:
-    print "Usage: python dummyFile.py IniYear FinYear TargetPath"
+if len(argv) != 5:
+    print "Usage: python dummyFile.py IniYear FinYear MODEL Scenario"
     exit(1)
-iniYear = int(argv[1])
-finYear = int(argv[2])
-TarPath = str(argv[3])
 
-for year in range(iniYear, finYear + 1):
-    fn = '%spr_day_CNRM-CM5_rcp85_r1i1p1_%s.nc' % (TarPath, year)
+IniYear = int(argv[1])
+FinYear = int(argv[2])
+MODEL = str(argv[3])
+Scen = str(argv[4])
+
+for year in range(2006, 2100):
+    fn = '/mnt/BCSD/%s/%s/day/r1i1p1/pr/pr_day_%s_%s_r1i1p1_%s.nc' % (MODEL, Scen, MODEL, Scen, year)
     if not path.exists(fn):
         print '... touch %s' % fn
         system('touch %s' % fn)
