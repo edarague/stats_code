95805a5b67e35da045cd2227f4c466eaf4bc6781
Edward Guevara <edarague@gmail.com>
2014-12-13 13:59:55 -0500

    cdo_main and stats_temp updated

--- BCSD_BAN_cut.py
+++ BCSD_BAN_cut.py
@@ -19,7 +19,7 @@ for fn in fi.readlines():
     if not path.exists(path.dirname(fo)):
         call('mkdir -p %s' % path.dirname(fn), shell=True)
         print '... directorio %s creado!' % path.dirname(fo)
-    system('ln -s %s inputFileTmp.nc' % fn)
+    call('ln -s %s inputFileTmp.nc' % fn, shell=True)
     if not path.exists(fo):
         print "... cut file Bangladesh: %s" % (fn)
         print 'nces -d lat,20.746231,26.631950 -d lon,88.028340,92.680664 %s %s' % ('inputFileTmp.nc', fo)
